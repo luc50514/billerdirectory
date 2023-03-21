1cea3f72b9d860d205fbda6236dbb57a7c2710e5
Jason McPeak <jmcpeak@payrailz.com>
2023-03-21 14:13:53 -0400

    get feed pushing at an interval

--- src/app/list/page.js
+++ src/app/list/page.js
@@ -1,16 +1,16 @@
 "use client";
 
-import * as React from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
 import Collapse from "@mui/material/Collapse";
-import IconButton from "@mui/material/IconButton";
+// import IconButton from "@mui/material/IconButton";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
-import DeleteIcon from "@mui/icons-material/Delete";
+// import DeleteIcon from "@mui/icons-material/Delete";
 import { TransitionGroup } from "react-transition-group";
-import useTimeout from "../useTimeout";
+import useInterval from "../useInterval";
 
 const EVENT_TYPES = [
   {
@@ -36,44 +36,44 @@ const EVENTS = [
     target: "Rich Hood",
     value: EVENT_TYPES[1],
   },
-]
+];
 
 function renderItem({ item }) {
   return (
     <ListItem>
-      <ListItemText primary={item.target} secondary={item.value.title}/>
+      <ListItemText primary={item.target} secondary={item.value.title} />
     </ListItem>
   );
 }
 
 export default function DrawerList() {
-  const [fruitsInBasket, setFruitsInBasket] = React.useState(
-    EVENTS
-  );
+  const [fruitsInBasket, setFruitsInBasket] = useState(EVENTS);
 
   const handleAddFruit = () => {
     setFruitsInBasket((items) => {
-      items.push({
-        id: items[items.length - 1].id + 1,
+      const id = items[0].id + 1;
+
+      items.unshift({
+        id,
         target: "Rich Hood",
         value: EVENT_TYPES[1],
-      })
-      return items;
-    })
+      });
+
+      return [...items];
+    });
   };
 
   const handleRemoveFruit = (item) => {
     setFruitsInBasket((prev) => [...prev.filter((i) => i !== item)]);
   };
 
-  useTimeout(handleAddFruit, 500)
+  useInterval(handleAddFruit, 2000);
 
   return (
     <div>
       <Box sx={{ mt: 1 }}>
         <List>
           <TransitionGroup>
-
             {fruitsInBasket.map((item) => (
               <Collapse key={item.id}>
                 {renderItem({ item, handleRemoveFruit })}
---
+++ src/app/useInterval.js
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from "react";
+
+/**
+ * useInterval - takes the hard work out of using setInterval/clearInterval in React
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval}
+ *
+ * @param {function} [callback=()=>{}] - A function to be executed every delay milliseconds.
+ * @param {number} [delay = 0] - The time, in milliseconds that the timer should wait before the specified function or code is executed.
+ * @param args - Additional arguments which are passed through to the function specified by func once the timer expires.
+ */
+const useInterval = (callback = () => {}, delay = 0, args) => {
+  const savedCallback = useRef(callback);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (delay === null || typeof delay !== "number") {
+      return () => {};
+    }
+
+    const tick = () => {
+      savedCallback.current();
+    };
+    const id = setInterval(tick, delay, args);
+
+    return () => {
+      clearInterval(id);
+    };
+  }, [args, delay]);
+};
+
+export default useInterval;
